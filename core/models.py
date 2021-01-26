994fef207d7404fd81480e5fa50ea09ec378408c
bamdadam <kanra252@gmail.com>
2021-01-26 21:17:48 +0330

    First commit

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User as DjangoUser
+
+
+# Create your models here.
+
+
+class SystemUser(models.Model):
+    user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE, null=True)
+    at_name = models.CharField(max_length=255)
+    retweeted_tweets = models.ManyToManyField("Tweet", null=True)
+    # pro_pic = models.ImageField(upload_to=get_image_upload_path, null=True)
+    relationship = models.ManyToManyField("SystemUser", through=RelationShip)
